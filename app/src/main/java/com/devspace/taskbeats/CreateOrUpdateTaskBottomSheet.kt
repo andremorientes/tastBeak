4e852157c8296daf75431049259da616f2eb69fc
André Morientes <59062594+andremorientes@users.noreply.github.com>
2024-12-26 13:39:32 +0200

    colocando acção de Click no task

--- app/src/main/java/com/devspace/taskbeats/CreateOrUpdateTaskBottomSheet.kt
+++ app/src/main/java/com/devspace/taskbeats/CreateOrUpdateTaskBottomSheet.kt
@@ -8,11 +8,13 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
-class CreateTaskBottomSheet(
+class CreateOrUpdateTaskBottomSheet(
+    private val task:TaskUiData?=null,
     private val categoryList: List<CategoryUiData>,
     private val onCreateCliked: (TaskUiData) -> Unit
 
@@ -23,38 +25,18 @@ class CreateTaskBottomSheet(
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.create_task_bottom_sheet, container, false)
+        val view = inflater.inflate(R.layout.create_or_update_task_bottom_sheet, container, false)
 
 
+        val tv_title_task= view.findViewById<TextView>(R.id.tv_create_task_title)
         val btn_CreatTask = view.findViewById<Button>(R.id.btn_create_task)
         val edt_task = view.findViewById<TextInputEditText>(R.id.edt_task)
-
-
-        var taskCategory: String? = null
-        btn_CreatTask.setOnClickListener {
-            val task = edt_task.text.toString()
-            if (taskCategory != null) {
-                onCreateCliked.invoke(
-
-                    TaskUiData(
-                        name = task,
-                        category = requireNotNull(taskCategory)
-                    )
-                )
-                dismiss()
-            } else {
-                Snackbar.make(
-                    btn_CreatTask,
-                    "Por favor Selecione uma Categoria",
-                    Snackbar.LENGTH_LONG
-                ).show()
-            }
-
-        }
-        val categoryString: List<String> = categoryList.map { it.name }
-
         val spinner: Spinner = view.findViewById(R.id.categoryList_spinner)
 
+
+        val categoryString: List<String> = categoryList.map { it.name }
+
+        var taskCategory: String? = null
         ArrayAdapter(
             requireActivity().baseContext,
             android.R.layout.simple_spinner_item,
@@ -81,6 +63,41 @@ class CreateTaskBottomSheet(
             }
 
         }
+
+
+        if (task== null){
+            tv_title_task.setText(R.string.create_title_task)
+            btn_CreatTask.setText(R.string.create)
+        }else{
+            tv_title_task.setText(R.string.update)
+            btn_CreatTask.setText(R.string.update)
+
+            edt_task.setText(task.name)
+        }
+
+
+        btn_CreatTask.setOnClickListener {
+            val task = edt_task.text.toString()
+            if (taskCategory != null) {
+                onCreateCliked.invoke(
+
+                    TaskUiData(
+                        id=0,
+                        name = task,
+                        category = requireNotNull(taskCategory)
+                    )
+                )
+                dismiss()
+            } else {
+                Snackbar.make(
+                    btn_CreatTask,
+                    "Por favor Selecione uma Categoria",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+
+        }
+
 
         return view
     }
--- app/src/main/java/com/devspace/taskbeats/TaskListAdapter.kt
+++ app/src/main/java/com/devspace/taskbeats/TaskListAdapter.kt
@@ -11,6 +11,12 @@ import androidx.recyclerview.widget.RecyclerView
 class TaskListAdapter :
     ListAdapter<TaskUiData, TaskListAdapter.TaskViewHolder>(TaskListAdapter) {
 
+    private lateinit var callBack: (TaskUiData) -> Unit
+
+     fun setOnClicklistner(onClick: (TaskUiData) -> Unit) {
+        callBack = onClick
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
@@ -19,17 +25,24 @@ class TaskListAdapter :
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
         val category = getItem(position)
-        holder.bind(category)
+        holder.bind(category,callBack)
+
+
     }
 
-    class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class TaskViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         private val tvCategory = view.findViewById<TextView>(R.id.tv_category_name)
         private val tvTask = view.findViewById<TextView>(R.id.tv_task_name)
 
-        fun bind(task: TaskUiData) {
+        fun bind(task: TaskUiData, callback:(TaskUiData)->Unit) {
             tvCategory.text = task.category
             tvTask.text = task.name
+
+            view.setOnClickListener {
+                callback.invoke(task)
+            }
         }
+
     }
 
     companion object : DiffUtil.ItemCallback<TaskUiData>() {
